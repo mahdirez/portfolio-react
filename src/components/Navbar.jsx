a1ffff3ec7d515dca2c807b9b467d3ad85e90c81
Mahdi Rezaei <mahdir798@gmail.com>
2024-03-17 16:18:26 +0330

    work section completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -91,7 +91,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://www.linkedin.com/in/mahdi-rezaei-7421a0258"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
@@ -99,7 +99,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://github.com/mahdirez"
             >
               Github <FaGithub size={30} />
             </a>
---
+++ src/data/data.js
@@ -0,0 +1,43 @@
+import blog from "../assets/projects/blog.png";
+import movie from "../assets/projects/movie-app.png";
+import snapp from "../assets/projects/snapp-site.png";
+import todo from "../assets/projects/todo-app.png";
+import mobile from "../assets/projects/mobile-shop.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "blog",
+    image: blog,
+    github: "",
+    live: "https://weblog-js-rezaei.netlify.app",
+  },
+  {
+    id: 2,
+    name: "movie Application",
+    image: movie,
+    github: "",
+    live: "https://movie-app-react-rezaei.netlify.app",
+  },
+  {
+    id: 3,
+    name: "snapp Application",
+    image: snapp,
+    github: "",
+    live: "https://snappwebpagerezaei.netlify.app",
+  },
+  {
+    id: 4,
+    name: "todo Application",
+    image: todo,
+    github: "",
+    live: "https://todoapp-react-js-mahdi-rezaei.netlify.app",
+  },
+  {
+    id: 5,
+    name: "mobile shop",
+    image: mobile,
+    github: "",
+    live: "https://mobile-shop101102.netlify.app",
+  },
+];
