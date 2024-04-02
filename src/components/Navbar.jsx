b8f5bcaa3c9f0a1ce5aecb367c96d60a514b99a1
Mahdi Rezaei <mahdir798@gmail.com>
2024-04-02 17:04:28 +0330

    fix resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { Link } from "react-scroll";
 import { saveAs } from "file-saver";
-// import "../../public/logo192.png";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
