523ed518f86edc7deee27bb61d7d6933a45b51c0
Mahdi Rezaei <mahdir798@gmail.com>
2024-04-02 03:35:53 +0330

    fix btn work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,11 +4,12 @@ import { data } from "../data/data.js";
 const Work = () => {
   // projects file
   const project = data;
+  //setProject(data);
 
   return (
     <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full ">
-        <div className="pb-2">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Work
           </p>
@@ -21,7 +22,7 @@ const Work = () => {
             <div
               key={index}
               style={{ backgroundImage: `url(${item.image})` }}
-              className="shadow-lg shadow-[#040c16]  container rounded-md 
+              className="shadow-lg shadow-[#040c16] group container rounded-md 
               flex justify-center text-center items-center mx-auto content-div "
             >
               {/* Hover effect for images */}
